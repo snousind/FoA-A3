bb1616488b8c54a4045108757309f6a65f26d495
ayad0010 <ayad0010@student.monash.edu>
2023-05-22 20:19:26 +1000

    Passing 1/2 test cases for Task 4

--- balancing.py
+++ balancing.py
@@ -2,4 +2,18 @@ from __future__ import annotations
 from threedeebeetree import Point
 
 def make_ordering(my_coordinate_list: list[Point]) -> list[Point]:
-    raise NotImplementedError()
+    def divide_points(points, axis):
+        points.sort(key=lambda p: p[axis])
+        mid = len(points) // 2
+        return points[mid], points[:mid], points[mid + 1:]
+
+    def create_ordered_list(points, axis):
+        if len(points) == 0:
+            return []
+        elif len(points) == 1:
+            return points
+        else:
+            root, left, right = divide_points(points, axis)
+            return [root] + create_ordered_list(left, (axis + 1) % 3) + create_ordered_list(right, (axis + 1) % 3)
+
+    return create_ordered_list(my_coordinate_list, 0)
