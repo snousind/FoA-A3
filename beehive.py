2cab6779463f6c07aea5200937539ba8605c06ea
ayad0010 <ayad0010@student.monash.edu>
2023-05-28 10:04:24 +1000

    Random commit

--- beehive.py
+++ beehive.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import List
 from heap import MaxHeap
 
 @dataclass
@@ -16,13 +17,35 @@ class Beehive:
 class BeehiveSelector:
 
     def __init__(self, max_beehives: int):
-        raise NotImplementedError()
+        self.max_beehives = max_beehives
+        self.beehives = MaxHeap(max_size=max_beehives)
 
     def set_all_beehives(self, hive_list: list[Beehive]):
-        raise NotImplementedError()
+        self.beehives.clear()
+        for hive in hive_list:
+            self.beehives.insert((-hive.capacity * hive.nutrient_factor, hive))
     
     def add_beehive(self, hive: Beehive):
-        raise NotImplementedError()
+        if len(self.beehives) < self.max_beehives:
+            self.beehives.insert((-hive.capacity * hive.nutrient_factor, hive))
+        else:
+            # Replace the beehive with the lowest emerald value if the heap is full
+            if self.beehives.root_key() < -hive.capacity * hive.nutrient_factor:
+                self.beehives.extract_root()
+                self.beehives.insert((-hive.capacity * hive.nutrient_factor, hive))
     
     def harvest_best_beehive(self):
-        raise NotImplementedError()
+        if not self.beehives:
+            return 0.0
+
+        _, best_hive = self.beehives.extract_root()
+        honey_taken = min(best_hive.capacity, best_hive.volume)
+        emeralds = honey_taken * best_hive.nutrient_factor
+        best_hive.volume -= honey_taken
+
+        if best_hive.volume > 0:
+            self.beehives.insert((-best_hive.capacity * best_hive.nutrient_factor, best_hive))
+
+        return emeralds
+
+
